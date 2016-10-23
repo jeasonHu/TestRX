ffda1d9759051070096301a5ed9bbc836e77db3f
AsherHome <softwareajax@163.com>
2016-10-23 17:43:47 +0800

    Signed-off-by: AsherHome <softwareajax@163.com>

--- app/src/main/java/com/hc/RXjava/MainActivity.java
+++ app/src/main/java/com/hc/RXjava/MainActivity.java
@@ -1,10 +1,12 @@
 package com.hc.RXjava;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,20 +15,21 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
+    CoordinatorLayout coordlayout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        coordlayout = (CoordinatorLayout)findViewById(R.id.ll_coordinatorlayout);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action ¡£¡£¡£¡£¡£", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own actionã€‚ã€‚ã€‚", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -48,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.i(TAG, "setting");
+            Snackbar.make(coordlayout, "setting", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
             return true;
         }
 
